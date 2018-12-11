75328b5230c26452d6164a03e75c384bf641a501
administrador <administrador@administradors-iMac.local>
2018-12-10 18:23:50 -0800

    Primera clase

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,79 @@
+import UIKit
+
+var str = "Hello, playground"
+str="bye, playground"
+print(str)
+
+let abc="clase de ios"
+print (abc)
+var num=1.3
+num=num+5
+print(num)
+let át="simbolos"
+print(át)
+
+let 🐷 = "gatito"
+print(🐷)
+
+struct Person {
+    let firstName:String
+    let lastName:String
+    var age:Int
+    
+    func sayHello(){
+        print("Hola mi nombre es \(firstName) \(lastName) ")
+        
+    }
+}
+
+let personaA=Person(firstName:"Jorge",
+                    lastName:"Pamo",age:10)
+personaA.sayHello()
+let number: Double=6
+print(number)
+
+let number1:Int=5
+print(number1)
+//let string : string
+//string="Inicializado"
+
+var score = 5
+score=score+4
+print(score)
+
+score+=4
+score-=3
+score*=3
+score/=10
+
+var x = 3
+var y = 5
+var z = 7
+x + y * z
+(x+y)*z
+
+//x + y
+// Operadores Logicos
+
+==
+    let temp=100
+if temp=100{
+print("el agua esta hirviendo)
+}else{
+    print("el agua no esta hirviendo"  )
+}
+var pos = 1
+if pos ==1{
+    print("felicitciones")
+}else if pos == 2{
+    print ("quedaste segundo")
+}else {
+    print("no ganaste")
+}
+ //boolean
+let nmb=100
+let esMenor=nmb 10000
+
+
+
+
